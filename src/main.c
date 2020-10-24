7fb345d37c61e1de9f21a5c1003d48bd85dfe295
geni <me@geni.site>
2020-10-24 10:57:19 +0300

    Avoid GUI ghosting

--- src/main.c
+++ src/main.c
@@ -64,10 +64,10 @@ int main(int argc, char** argv) {
 				case SDL_QUIT: c.running = false; break;
 				case SDL_KEYUP: {
 					if (event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
-						/* Draw the CHIP8 screen if the GUI is being closed and the delay timer is above 0
-						   If the delay timer is above 0, the CHIP8 screen won't be drawn until it gets down to 0 and a DXYN opcode is executed
+						/* Draw the CHIP8 screen if the GUI is being closed and the delay timer is above 0 or the CPU is halted
+						   If not done, the CHIP8 screen won't be drawn until it gets down to 0 and a DXYN opcode is executed
 						   This results in the GUI still being on the screen, despite being closed */
-						if (gui.open && c.delay > 0) {
+						if (gui.open && (c.delay > 0 || c.cpu.halt)) {
 							chip8_screen_draw(&c.screen);
 						}
 
@@ -84,7 +84,10 @@ int main(int argc, char** argv) {
 		nk_input_end(gui.ctx);
 
 		if (gui.open) {
-			chip8_screen_draw(&c.screen);
+			/* Draw the CHIP8 screen if the delay timer is over 0 or if the CPU is halted to avoid GUI ghosting */
+			if (c.delay > 0 || c.cpu.halt) {
+				chip8_screen_draw(&c.screen);
+			}
 
 			camus_gui_update(&gui);
 			camus_gui_draw(&gui);
