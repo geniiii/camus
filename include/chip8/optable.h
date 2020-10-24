03f0097b44f50b518239975c477e53c86931f375
geni <me@geni.site>
2020-10-24 10:56:06 +0300

    Remove unnecessary optable declarations

--- include/chip8/optable.h
+++ include/chip8/optable.h
@@ -6,19 +6,8 @@
 #define OP(op) extern void chip8_op_##op(struct chip8* restrict c);
 
 OP(0xxx)
-OP(1xxx)
-OP(2xxx)
-OP(3xxx)
-OP(4xxx)
-OP(5xxx)
-OP(6xxx)
-OP(7xxx)
 OP(8xxx)
 OP(9xxx)
-OP(axxx)
-OP(bxxx)
-OP(cxxx)
-OP(dxxx)
 OP(exxx)
 OP(fxxx)
 
