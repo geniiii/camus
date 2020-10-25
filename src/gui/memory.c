959cdeda9346413fa500dd85d5f2b2979b5fec1f
geni <me@geni.site>
2020-10-25 15:09:11 +0200

    Actually display 16 bytes per row, clean up

--- src/gui/memory.c
+++ src/gui/memory.c
@@ -3,38 +3,42 @@
 #define NAME "Memory"
 
 bool camus_gui_memory(struct camus_gui* restrict g) {
-	if (nk_begin(g->ctx, NAME, nk_rect(50, 50, 580, 600), NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_CLOSABLE | NK_WINDOW_NO_SCROLLBAR)) {
+	if (nk_begin(g->ctx, NAME, nk_rect(50, 50, 350, 400), NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_CLOSABLE | NK_WINDOW_NO_SCROLLBAR)) {
 		const struct nk_rect total_space   = nk_window_get_content_region(g->ctx);
 		const float			 panel_padding = g->ctx->style.window.padding.x;
 
-		nk_layout_row_begin(g->ctx, NK_STATIC, 10, 0xF + 1);
+		nk_layout_row_begin(g->ctx, NK_STATIC, 10, 1 + 0x8);
 		{
 			nk_layout_row_push(g->ctx, 35 + panel_padding);
 			nk_spacing(g->ctx, 1);
 
 			for (u8 i = 0; i <= 0xF; i += 2) {
-				char hex[2 + 1 + 1];
-				sprintf(hex, "%02X ", i);
-				nk_layout_row_push(g->ctx, 60 + panel_padding);
+				char hex[2 + 1];
+				sprintf(hex, "%02X", i);
+
+				nk_layout_row_push(g->ctx, 30);
 				nk_label(g->ctx, hex, NK_TEXT_ALIGN_LEFT);
 			}
 		}
 		nk_layout_row_end(g->ctx);
 
-		nk_layout_row_static(g->ctx, total_space.h - panel_padding, total_space.w, 1);
+		nk_layout_row_static(g->ctx, total_space.h - panel_padding * 2, total_space.w, 1);
 		if (nk_group_begin(g->ctx, "Contents", 0)) {
-			nk_layout_row_begin(g->ctx, NK_STATIC, 10, 0xF + 1);
+			nk_layout_row_begin(g->ctx, NK_STATIC, 10, 1 + 0x8);
 			{
-				for (u16 i = 0; i < MEM_SIZE; ++i) {
-					char hex[4 + 1 + 1];
+				for (u16 i = 1; i < MEM_SIZE + 1; i += 2) {
+					char hex[4 + 1];
 
-					if (i % 0x10 == 0) {
+					if ((i - 1) % 0x10 == 0) {
 						nk_layout_row_push(g->ctx, 35);
-						sprintf(hex, "%04X ", i);
-					} else {
-						nk_layout_row_push(g->ctx, 30);
-						sprintf(hex, "%04X ", g->c->mem[i] | g->c->mem[i + 1] << 8);
+						sprintf(hex, "%04X", i - 1);
+
+						nk_label(g->ctx, hex, NK_TEXT_ALIGN_LEFT);
 					}
+
+					nk_layout_row_push(g->ctx, 30);
+					sprintf(hex, "%04X", g->c->mem[i - 1] << 8 | g->c->mem[i]);
+
 					nk_label(g->ctx, hex, NK_TEXT_ALIGN_LEFT);
 				}
 			}
